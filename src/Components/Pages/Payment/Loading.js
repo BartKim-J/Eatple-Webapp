ff0498a0753dd6a2b5c271b8dc07f0cac6ff0d2c
jaehwankim07120@gmail.com <jaehwankim07120@gmail.com>
2019-10-08 14:57:26 +0900

    payment update

---
+++ src/Components/Pages/Payment/Loading.js
@@ -0,0 +1,20 @@
+// Standard Include
+import React, { Component } from 'react';
+
+// Components
+import './Loading.scss';
+
+class Payment extends Component {
+    render() {
+      return (
+        <div className="payment">
+          <header className="payment-header">
+  
+            <h1 className="payment-title">Membership Page.</h1>
+          </header>
+        </div>
+      );
+    }
+  }
+  
+  export default Payment;
