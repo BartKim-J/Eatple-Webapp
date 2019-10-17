bfd0873ea17af3ce1c8a7160b14a31ab9ecc9133
jaehwankim07120@gmail.com <jaehwankim07120@gmail.com>
2019-10-17 18:07:16 +0900

    add main contents demo

--- src/Components/Pages/Homepage/Contents/Main/index.jsx
+++ src/Components/Pages/Homepage/Contents/Main/index.jsx
@@ -1,32 +1,44 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useState } from 'react';
 
 import './index.scss';
 
-export default class ContentMain extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+import imgBtnLeft from './Images/BtnLeft.png';
+import imgBtnRight from './Images/BtnRight.png';
+import imgBtnFree from './Images/BtnFree.png';
+import imgSlider from './Images/Slider-1.png';
 
-  componentDidMount() {}
-
-  render() {
-    return (
-      <div className="content-main">
-        <div className="content-main-inner">
+export default function CotentMain() {
+  return (
+    <div className="content-main">
+      <div className="content-main-inner">
+        <div className="main-head-wrapper">
           <div className="main-head-box">
-            <div className="text-box">
-              <div className="title">
-                한끼 식사에
-                <br />
-                가치를 더하다, 
-                <span className="highlight"> 잇플</span>
-                <div className="sub-text">잇플은 한끼 식사를 5500원에 해결할 수 있는 테이크아웃 멤버쉽입니다.</div>
+            <div className="title">
+              한끼 식사에
+              <br />
+              가치를 더하다,
+              <span className="highlight"> 잇플</span>
+              <div className="sub-text">
+                잇플은 한끼 식사를 5500원에 해결할 수 있는 테이크아웃 멤버쉽입니다.
               </div>
             </div>
           </div>
         </div>
+
+        <div className="main-slide-wrapper">
+          <div className="main-slide-box">
+            <img src={imgSlider} alt="slide-1" />
+          </div>
+
+          <button type="button" className="main-slide-btn-left">
+            <img src={imgBtnLeft} alt="BtnRight" />
+          </button>
+
+          <button type="button" className="main-slide-btn-right">
+            <img src={imgBtnRight} alt="BtnLeft" />
+          </button>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
