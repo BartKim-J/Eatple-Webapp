1274127990dd1c3ff1510ad6140e586dd6eb1119
jaehwankim07120@gmail.com <jaehwankim07120@gmail.com>
2019-10-23 15:56:36 +0900

    calculator content form added '

---
+++ src/Components/Pages/Homepage/Contents/Calculator copy/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+export default function Calculator() {
+  return (
+    <div className="content-calculator-section">
+      <div className="content-calculator-inner">
+        <div className="content-calculator-box-wrap">
+          <div className="content-calculator-box" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/Components/Pages/Homepage/Contents/Tutorial/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './index.scss';
+
+export default function Tutorial() {
+  return (
+    <div className="content-tutorial-section">
+      <div className="content-tutorial-inner">
+        <div className="content-tutorial-box-wrap">
+          <div className="content-tutorial-box" />
+        </div>
+      </div>
+    </div>
+  );
+}
