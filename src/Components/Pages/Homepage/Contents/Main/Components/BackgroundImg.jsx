280d1d16c086aaf67f7c594ddd04588a7742217e
jaehwankim07120@gmail.com <jaehwankim07120@gmail.com>
2019-11-04 18:58:48 +0900

    main background styled update

--- src/Components/Pages/Homepage/Contents/Main/Components/BackgroundImg.jsx
+++ src/Components/Pages/Homepage/Contents/Main/Components/BackgroundImg.jsx
@@ -33,17 +33,15 @@ Styled.Container = styled.div`
 
   width: 100%;
   height: 100%;
-
-
 `;
 Styled.ImageBox = styled.div`
   width: 100%;
   height: 100%;
 
   overflow: hidden;
-  
+
   img {
     min-width: 100%;
-    min-height: 100%;
+    height: 100%;
   }
 `;
