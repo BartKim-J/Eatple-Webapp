97d559eec1fcc52d3226cf67068b32b9511dd250
Bart Kim <jaehwankim07120@gmail.com>
2019-11-20 12:34:07 +0900

    firebase added

--- src/Components/Pages/Homepage/Contents/Membership/Components/HeroImg.jsx
+++ src/Components/Pages/Homepage/Contents/Membership/Components/HeroImg.jsx
@@ -11,6 +11,7 @@ export default function HeroImg() {
   return (
     <Styled.Wrap>
       <Styled.Container scrollY={scrollY} offsetY={window.innerHeight}>
+        <Styled.Mask />
         <Styled.ImageBox src={ImgHero} scrollY={scrollY} offsetY={window.innerHeight} />
       </Styled.Container>
     </Styled.Wrap>
@@ -43,12 +44,25 @@ Styled.Container = styled.div`
   filter: ${props => {
     let value = 100;
 
-    if (props.scrollY > props.offsetY / 2) {
+    if (props.scrollY > props.offsetY * 0.7) {
       value = 0;
     }
 
     return `grayscale(${value}%)`;
   }};
+`;
+
+Styled.Mask = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  width: 100%;
+  height: 60%;
+
+  background: linear-gradient(to bottom, transparent 0%, black 100%);
+
+  z-index: 100;
 `;
 
 Styled.ImageBox = styled.div`
@@ -61,7 +75,7 @@ Styled.ImageBox = styled.div`
 
   transition: 0.6s ease-out;
   filter: ${props => {
-    let value = 5;
+    let value = 3;
 
     if (props.scrollY > props.offsetY * 0.7) {
       value = 0;
